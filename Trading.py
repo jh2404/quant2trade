f9fbe65f2d44da3ea4162029770c1ce2694a365c
JHYUN's GH <yoon2404@gmail.com>
2023-09-18 21:48:57 +0900

    First Commit to main
    시작!

---
+++ Trading.py
@@ -0,0 +1,37 @@
+import sys                        # system specific parameters and functions : 파이썬 스크립트 관리
+from PyQt5.QtWidgets import *     # GUI의 그래픽적 요소를 제어       하단의 terminal 선택, activate py37_32,  pip install pyqt5,   전부다 y
+from PyQt5 import uic             # ui 파일을 가져오기위한 함수
+
+
+################# 부가 기능 수행(일꾼) #####################################
+from kiwoom import Kiwoom          # 키움증권 함수/공용 방 (싱글턴)
+
+#=================== 프로그램 실행 프로그램 =========================#
+
+form_class = uic.loadUiType("TRADER.ui")[0]             # 만들어 놓은 ui 불러오기
+
+class Login_Machnine(QMainWindow, QWidget, form_class):       # QMainWindow : PyQt5에서 윈도우 생성시 필요한 함수
+
+    def __init__(self, *args, **kwargs):                      # Main class의 self를 초기화 한다.
+
+        print("Login Machine 실행합니다.")
+        super(Login_Machnine, self).__init__(*args, **kwargs)
+        form_class.__init__(self)                            # 상속 받은 from_class를 실행하기 위한 초기값(초기화)
+        self.setUI()                                         # UI 초기값 셋업 반드시 필요
+
+        ####키움증권 로그인 하기
+        self.k = Kiwoom()                     # Kiwoom()을 실행하며 상속 받는다. Kiwoom()은 전지적인 아이다.
+
+    def setUI(self):
+        self.setupUi(self)                # UI 초기값 셋업
+        
+        
+#======================================================================#
+
+if __name__=='__main__':             # import된 것들을 실행시키지 않고 __main__에서 실행하는 것만 실행 시킨다.
+                                     # 즉 import된 다른 함수의 코드를 이 화면에서 실행시키지 않겠다는 의미이다.
+
+    app = QApplication(sys.argv)     # PyQt5로 실행할 파일명을 자동으로 설정, PyQt5에서 자동으로 프로그램 실행
+    CH = Login_Machnine()            # Main 클래스 myApp으로 인스턴스화
+    CH.show()                        # myApp에 있는 ui를 실행한다.
+    app.exec_()                      # 이벤트 루프``
---
+++ kiwoom.py
@@ -0,0 +1,16 @@
+
+from PyQt5.QtWidgets import *                 # GUI의 그래픽적 요소를 제어       하단의 terminal 선택, activate py37_32,  pip install pyqt5,   전부다 y
+from PyQt5.QAxContainer import *              # 키움증권의 클레스를 사용할 수 있게 한다.(QAxWidget)
+from PyQt5Singleton import Singleton
+
+class Kiwoom(QWidget, metaclass=Singleton):       # QMainWindow : PyQt5에서 윈도우 생성시 필요한 함수
+
+    def __init__(self, parent=None, **kwargs):                    # Main class의 self를 초기화 한다.
+
+        print("로그인 프로그램을 실행합니다.")
+
+        super().__init__(parent, **kwargs)
+
+        ################ 로그인 관련 정보
+
+        self.kiwoom = QAxWidget('KHOPENAPI.KHOpenAPICtrl.1')       # CLSID
